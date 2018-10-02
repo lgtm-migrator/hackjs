2feda9daf4142e8752562f83fe86d9a783ab9623
Dennis Hedegaard <dennis@dhedegaard.dk>
2018-10-02 22:05:44 +0200

    Improved comments on a test case.

--- src/memory/ram.spec.ts
+++ src/memory/ram.spec.ts
@@ -5,17 +5,17 @@ describe("ram", () => {
     it("can load some data, keep it, and return it later", () => {
       const reg = Register();
 
-      // Load a bit.
+      // Store a bit, with a bit load bit..
       expect(reg(1, 1)).toEqual(1);
 
       // Fetch the stored bit, expect it to still be 1, while the load bit is low.
       expect(reg(0, 0)).toEqual(1);
       expect(reg(1, 0)).toEqual(1);
 
-      // Load a new state.
+      // Load a new state, with a high load bit.
       expect(reg(0, 1)).toEqual(0);
 
-      // Test that the state is kept, as expected.
+      // Test that the new state is still kept, while the load bit is low.
       expect(reg(0, 0)).toEqual(0);
       expect(reg(1, 0)).toEqual(0);
     });

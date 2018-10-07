c84fcc951a8c2469b3561eae3673a094ed752db0
Dennis Hedegaard <dennis@dhedegaard.dk>
2018-10-07 21:54:26 +0200

    memory: Small fixes.

--- src/memory/ram.ts
+++ src/memory/ram.ts
@@ -37,10 +37,9 @@ export const Register = () => {
   // Assemble 16 registers.
   const output = [...Array(16)].map(() => BitRegister());
 
-  return (input: Bit16, load: Bit): Bit16 => {
+  return (input: Bit16, load: Bit): Bit16 =>
     // Call all the registers with each input and the load bit.
-    return output.map((reg, idx) => reg(input[idx], load)) as Bit16;
-  };
+    output.map((reg, idx) => reg(input[idx], load)) as Bit16;
 };
 
 /**
@@ -53,10 +52,10 @@ export const Ram8 = () => {
 
   return (input: Bit16, address: Bit3, load: Bit): Bit16 => {
     // Start by demuxifying the load bits for each register.
-    const loads = DMux8Way(load, address);
+    const loadbits = DMux8Way(load, address);
     // Call all the registers with the demuxified load bit.
     const output = memory.map((register, idx) =>
-      register(input, loads[idx]));
+      register(input, loadbits[idx]));
     // Multiplex the output to make sure the correct address is returned.
     return Mux8Way16(
       output[0], output[1], output[2], output[3],
